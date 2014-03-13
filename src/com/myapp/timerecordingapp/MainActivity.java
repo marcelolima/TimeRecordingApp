673d1d72df6c72a61c4b8e282982363a33b25bea
Marcelo Lima <marcelo.lima@openbossa.org>
2014-03-13 20:15:38 -0300

    VIEW: Add "New task" button to the Main Activity

--- src/com/myapp/timerecordingapp/MainActivity.java
+++ src/com/myapp/timerecordingapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.myapp.timerecordingapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,18 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Button buttonNewTask = (Button) findViewById(R.id.button_new_task);
+
+		buttonNewTask.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent("com.myapp.timerecordingapp.NEWTASK");
+				startActivity(intent);
+			}
+		});
+
 	}
 
 	@Override
