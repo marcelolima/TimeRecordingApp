b18176d2f6fd64a0ff1beeaeadcd484a4659e8a6
Marcelo Lima <marcelo.lima@openbossa.org>
2014-03-19 19:34:35 -0300

    ACTIVITY: Checkin on Task creation, if connected.
    Automatically do a checkin on the created task if the phone
    is already connected to the chosen wifi.

--- src/com/myapp/timerecordingapp/NewTaskActivity.java
+++ src/com/myapp/timerecordingapp/NewTaskActivity.java
@@ -1,7 +1,12 @@
 package com.myapp.timerecordingapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -63,6 +68,18 @@ public class NewTaskActivity extends Activity {
 					Toast toast = Toast.makeText(getApplicationContext(), "Task name already exists", Toast.LENGTH_SHORT);
 					toast.show();
 					return;
+				}
+
+				// Create a record if the current connected wifi is the same
+				ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+				NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+				if (mWifi.isConnected()) {
+					WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+					WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+
+					if (ssid.equals(wifiInfo.getSSID().replace("\"", "")) && bssid.equals(wifiInfo.getBSSID()))
+						db.addCheckIn(idTask);
 				}
 
 				db.close();
